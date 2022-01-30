752e11a6a57b32271421d7295b819142ba2bf93e
MissingNewt <chelby.sallady@gmail.com>
2022-01-30 00:02:29 -0800

    adding readme file

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
 # Note-Taker
+
+## Description
+
+The goal with this application was to get used to express routes and creating post, delete and get routes. It was challenging connecting code to something that was already partially written, but through looking at different sources and examples, and some help from my tutors, I got the application running. 
+
+## Installation
+In order to install the code for this application, you'll need node.js and some sort of text editor. Run npm i in the commandline and then nodemon if you have it, for easy editing with a server. Then run npm start in the commandline to start up the server and open up your browser onto localhost:3002/ 
+## Usage
+To use this application, click the get started button to go to the notes part of the application. 
+![get started button](./11:Readme/README-images/1st.png)
+Once you click on the get started button you will be brought to the main note taking page. 
+![main note taking page](./11:Readme/README-images/2nd.png)
+click on where it says note title and then the note text, then start typing whatever you want into the note. 
+![typing into note section](./11:Readme/README-images/3rd.png)
+Once you have finished typing your new note, click the floppy disk on the top right corner to save your new note. 
+![floppy disk icon](./11:Readme/README-images/4th.png)
+Once you've clicked the floppy disk icon then your new note will appear on the left hand side of the page, for later reading. 
+![notes list](./11:Readme/README-images/5th.png)
+
+[Link to deployed application](https://chelbys-note-taker.herokuapp.com/)
+
+## Credits
+My tutor Eric, and some repositories from other bootcamps for the same assignment, I probably looked at at least 20. 
+
+## License
+MIT License
+
+Copyright (c) [2022] [chel
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-// const database = require('./db/db.json');
+const db = './db/db.json';
 const uuid = require('uuid');
-// const api = require('public/assets/js/routes.js');
+
+
 const app = express();
 const PORT =  process.env.PORT || 3002;
 
@@ -11,73 +12,61 @@ const PORT =  process.env.PORT || 3002;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
-// app.use('/api', api);
 
-//Setting up the URL routes
-app.get("/notes", function(req, res) {
+//-----------routes-------------------
+app.get("/notes", (req, res)=>{
     res.sendFile(path.join(__dirname , "./public/notes.html"));
 });
-app.get("/", function(req, res) {
+app.get("/", (req, res)=>{
     res.sendFile(path.join(__dirname, "./index.html"));
 });
 
-//Display 
+//setting up getpostdelete routes --------------------------------
 app.get("/api/notes", (req, res) => {
-    fs.readFile(path.join(__dirname, "./db/db.json"), (err, data) => {
-        if (err) throw err;
+    fs.readFile(path.join(__dirname, db), (err, data) => {
+        if (err) console.log(`GET Error: ${err}`);
         const notes = JSON.parse(data);
         res.json(notes);
     })
 });
 
-//New Note
-app.post("/api/notes", function(req, res) {
-    // res.send('works')
-    fs.readFile(path.join(__dirname, "./db/db.json"), (err, data) => {
-        if (err) throw err;
+app.post("/api/notes", (req, res)=>{
+    // res.send('this route works')
+    fs.readFile(path.join(__dirname, db), (err, data) => {
+        if (err) console.log(`1 POST Error: ${err}`);
         const notes = JSON.parse(data);
         const newNote = req.body;
         newNote.id = uuid.v4();
         notes.push(newNote);
+        const newNotesList = JSON.stringify(notes);
 
-        const createNote = JSON.stringify(notes);
-        fs.writeFile(path.join(__dirname, "./db/db.json"), createNote, (err) =>{
-            if (err) throw err;
+        fs.writeFile(path.join(__dirname, db), newNotesList, (err) =>{
+            if (err) console.log(`2 POST Error: ${err}`);
         });
         res.json(newNote);
     });
 });
 
-//Delete Saved Notes
-app.delete("/api/notes/:id", function(req, res) {
+app.delete("/api/notes/:id", (req, res)=>{
+//     console.log(`attempting to delete`)
     const noteID = req.params.id;
-    fs.readFile(path.join(__dirname, "./db/db.json"), (err, data) => {
-        if (err) throw err;
+    fs.readFile(path.join(__dirname, db), (err, data) => {
+        if (err) console.log(`1 DELETE Error: ${err}`);
         const notes = JSON.parse(data);
-        const notesArray = notes.filter(item => {
-            return item.id !== noteID
+        const notesList = notes.filter((remainingNotes) => {
+            return remainingNotes.id !== noteID
         });
-        fs.writeFile('./db/db.json', JSON.stringify(notesArray), (err, data) => {
-            console.log("Delete")
-            if (err) throw err; 
-            res.json(notesArray) 
+        fs.writeFile(db, JSON.stringify(notesList), (err, data) => {
+            console.log("Deleted note successfully!")
+            if (err) console.log(`2 DELETE Error: ${err}`); 
+            res.json(notesList) 
 
         });
     });
 
 });
 
-//--------------------------------------------------------------------------------------------------------
-
-app.get('/', (req, res) => res.send('Navigate to /api/notes'));
-
-app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, './public/api/notes.html'))
-);
-
-
-
 app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT}`)
+  console.log(`App listening at port ${PORT}`)
 );
 
